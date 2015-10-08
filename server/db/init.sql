88183c93def88d0be510e890000e18c216cd8be1
happymap <nafan628@gmail.com>
2015-10-08 01:18:48 -0700

    add package.json and add sql scripts to create truck and schedule tables

---
+++ server/db/init.sql
@@ -0,0 +1,22 @@
+use food_truck;
+create table truck (
+	truck_id int not null auto_increment,
+    name char(50) not null,
+    description varchar(255) null,
+    primary key (truck_id)
+);
+
+create table schedule (
+	schedule_id int not null auto_increment,
+    start_time datetime not null,
+    end_time datetime not null,
+    address text null,
+    city char(50),
+    state char(50),
+    zip char(50),
+    display_address varchar(255),
+    longtitude float not null,
+    latitude float not null,
+    truck_id int not null,
+    primary key (schedule_id)
+);
