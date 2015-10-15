7ea713c9391fa34d4f7c6f13fd57a90911438c3b
Yifan Ying <yying@fitbit.com>
2015-10-15 10:30:36 -0700

    finished japa curry spider

--- scripts/schedules/schedules/spiders/japacurry_spider.py
+++ scripts/schedules/schedules/spiders/japacurry_spider.py
@@ -1,37 +1,39 @@
 import scrapy
 import pytz
-import datetime
 
 from schedules.items import SchedulesItem
+from datetime import datetime, timedelta
 
 class JapaCurrySpider(scrapy.Spider):
 	name = "japacurry"
 	start_urls = ["http://japacurry.com/"];
 
-	tz = "America/Los_Angeles"
-	time_delimiter = "-"
-
 	def parse(self, response):
-
+		time_delimiter = "-"
 
 		for schedule in response.xpath('//dl[@class="schedule-grid"]'):
 			date = schedule.xpath('dt[@class="date"]/text()').extract()[0]
 			times = schedule.xpath('dd[@class="time"]/text()').extract()[0]
+			times = times.split(time_delimiter)
 			address = schedule.xpath('dd[@class="place"]/text()').extract()[0]
 			city = "San Francisco"
 			state = "CA"
 
-			start_time = 
-
 			item = SchedulesItem()
 
+			if (date and times and len(times) == 2):
+				date = date.strip()
+				start_time = datetime.strptime("2015/" + date + " " + times[0].strip(), "%Y/%m/%d %H:%M")
+				end_time = datetime.strptime("2015/" + date + " " + times[1].strip(), "%Y/%m/%d %H:%M")
 
-			print date, time, address
+				# if start_time is after the end_time, means time is in 12-hour format
+				if (start_time > end_time):
+					end_time += timedelta(hours=12)
 
+				item['start_time'] = start_time
+				item['end_time'] = end_time
+				item['address'] = address
+				item['city'] = city
+				item['state'] = state
 
-	# date: 10/15
-	# time: 11:00
-	def convertDateToTimestamp(self, date, time):
-		naive_date = datetime.datetime.strptime("2015/" + date + " " + time, "%Y/%m/%d %H:%M")
-		localtz = pytz.timezone(tz)
-		print(localtz.localize(naive_date))
+				yield item
