c812b8104ea65f293de20abc43185af3722a5de7
happymap <nafan628@gmail.com>
2015-10-16 00:21:50 -0700

    initialize ios app

---
+++ client/foodtruck/foodtruck/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  foodtruck
+//
+//  Created by Yifan Ying on 10/16/15.
+//  Copyright © 2015 Yifan Ying. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Value2, reuseIdentifier: nil)
+        return cell;
+    }
+}
+
