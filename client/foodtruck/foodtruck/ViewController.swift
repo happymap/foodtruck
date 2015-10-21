a88b7940f90e0a8b207abfaac0c8f842c205a146
happymap <nafan628@gmail.com>
2015-10-20 23:45:08 -0700

    add http request to fetch trucks

--- client/foodtruck/foodtruck/ViewController.swift
+++ client/foodtruck/foodtruck/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // get list of trucks
+        let url = NSURL(string: "http://ngtemplates.com:3000/truck/list")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            for element: AnyObject in self.JSONParseArray(NSString(data: data!, encoding:NSUTF8StringEncoding) as! String) {
+                let name = element["name"] as? String
+                let distance = element["distance"] as? Float
+                
+                print(name)
+                print(distance)
+            }
+        }
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,7 +41,21 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.Value2, reuseIdentifier: nil)
-        return cell;
+        return cell
+    }
+    
+    // convert string to json object
+    func JSONParseArray(string: String) -> [AnyObject]{
+        if let data = string.dataUsingEncoding(NSUTF8StringEncoding) {
+            do {
+                if let array = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) as? [AnyObject] {
+                    return array
+                }
+            } catch {
+                print("error")
+            }
+        }
+        return [AnyObject]()
     }
 }
 
