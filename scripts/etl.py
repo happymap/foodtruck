9906903ec0174ec9fb88df912b2e1567ce2941c6
happymap <nafan628@gmail.com>
2016-03-16 01:06:06 -0700

    add hour parser

--- scripts/etl.py
+++ scripts/etl.py
@@ -2,8 +2,17 @@
 
 import csv
 import copy
+import re
 
-DAYS_LIST = {"mo":0, "tu":0, "we":0, "th":0, "fr":0, "sa":0, "su":0}
+DAYS_COUNTER = {"mo":0, "tu":0, "we":0, "th":0, "fr":0, "sa":0, "su":0}
+DAYS_LIST = ["mo", "tu", "we", "th", "fr", "sa", "su"]
+
+# examples:
+# 1. Mo-Su:10AM-6PM
+# 2. Tu/Th/Fr:9AM-3PM
+# 3. Sa/Sa/Sa/Sa/Sa:12AM-1AM;Mo/Mo/Mo/Mo/Mo:6AM-7AM/8AM-9AM;Su/Su/Su/Su/Su:9AM-10AM;Mo/Mo/Mo/Mo/Mo:9AM-1
+# 4. Tu:11AM-2PM
+# 5. Th:11AM-1PM;We:12PM-2PM;Mo/Fr:12PM-4PM;Tu:1PM-4PM
 
 def dates_parser(dates):
 	result = []
@@ -11,34 +20,53 @@ def dates_parser(dates):
 		days = dates.split('-')
 		start = False
 		end = False
-		for day,index in DAYS_LIST.iteritems():
-			if day.lower() == days[0].lower():
-				if start == False and end == False:
-					result.append(day)
-					start = True
-				elif start == True and end == False:
-					result.append(day)
-					end = True
+		for day in DAYS_LIST:
+			if end == True:
+				break
 			if start == True and end == False:
 				result.append(day)
-			elif end == True:
-				break
-			elif start == False and end == False:
-				continue
-
+			if start == False and day.lower() == days[0].lower():
+				result.append(day)
+				start = True
+			elif start == True and day.lower() == days[1].lower():
+				end = True
 	elif '/' in dates:
 		days = dates.split('/')
-		days_list_copy = copy.deepcopy(DAYS_LIST)
+		days_counter_copy = copy.deepcopy(DAYS_COUNTER)
 		for day in days:
-			if day.lower() in days_list_copy:
-				days_list_copy[day.lower()] += 1
-		for key,count in days_list_copy.iteritems():
+			if day.lower() in days_counter_copy:
+				days_counter_copy[day.lower()] += 1
+		for key,count in days_counter_copy.iteritems():
 			if count > 0:
 				result.append(key)
 	elif dates.lower() in DAYS_LIST:
 		result.append(dates.lower())
 
-	print result
+	return result
+
+def hours_parser(hours):
+	result = []
+	events = hours.split('/')
+	for event in events:
+		pair = []
+		start_and_end = event.split('-')
+		if len(start_and_end) != 2:
+			continue
+		start = get_actual_hour(start_and_end[0].strip())
+		end = get_actual_hour(start_and_end[1].strip())
+		pair.append(start)
+		pair.append(end)
+		result.append(pair)
+	return result
+		
+def get_actual_hour(hour_str):
+	num = int(re.findall(r'\d+', hour_str)[0])
+	if 'PM' in hour_str:
+		return num + 12
+	elif 'AM' in hour_str:
+		return num
+	else:
+		return -1
 
 
 with open('Mobile_Food_Facility_Permit.csv') as csvfile:
@@ -58,14 +86,9 @@ with open('Mobile_Food_Facility_Permit.csv') as csvfile:
 			if len(parts) != 2:
 				continue
 			dates = parts[0]
-			times = parts[1]
+			hours = parts[1]
 
-			dates_parser(dates)
+			print dates_parser(dates)
+			print hours_parser(hours)
 
-		# examples:
-		# 1. Mo-Su:10AM-6PM
-		# 2. Tu/Th/Fr:9AM-3PM
-		# 3. Sa/Sa/Sa/Sa/Sa:12AM-1AM;Mo/Mo/Mo/Mo/Mo:6AM-7AM/8AM-9AM;Su/Su/Su/Su/Su:9AM-10AM;Mo/Mo/Mo/Mo/Mo:9AM-1
-		# 4. Tu:11AM-2PM
-		# 5. Th:11AM-1PM;We:12PM-2PM;Mo/Fr:12PM-4PM;Tu:1PM-4PM
 
